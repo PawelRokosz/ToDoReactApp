4d8a37c36da3feac4d9c5b86be29e10e2549503d
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-09-23 18:12:13 +0200

    task done icon patch

--- src/components/Task.js
+++ src/components/Task.js
@@ -38,8 +38,10 @@ class Task extends React.Component {
 
    return (
     <div className={`tasks__task ${doneClass}`} onClick={() => this.handleDoneClick()}>
-      <div className="tasks__indicator">{doneIcon}</div>
-      <span className={`tasks__item ${doneItem}`}>{this.props.item}</span>
+      <div className="tasks__info">
+        <div className="tasks__indicator">{doneIcon}</div>
+        <span className={`tasks__item ${doneItem}`}>{this.props.item}</span>
+      </div>
       <div className="tasks__options">
         <div className="tasks__edit" onClick={(e) => this.handleEditClick(e)}><i className="fa fa-pencil-square-o" aria-hidden="true"></i></div>
         <div className="tasks__delete" onClick={(e) => this.handleDeleteClick(e)}><i className="fa fa-trash-o" aria-hidden="true"></i></div>
